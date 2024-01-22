dee067ff7039a392b9ad4517148b069bfd09128a
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-01-22 15:49:46 +0900

    Title: 정수 선언하고 곱 출력, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240122/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        a = 26;
+        b = 5;
+        System.out.printf("%d * %d = %d", a, b, a*b);
+    }
+}
