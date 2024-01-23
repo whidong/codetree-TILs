fe41ff2834a91917f082e4f1a3b3cd0e923544e1
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-01-23 17:07:18 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240123/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a, b ;
+    a = 1;
+    b = 2;
+    string c = "C";
+    cout << a << "->"<< b <<"->" << c;
+    return 0;
+}
