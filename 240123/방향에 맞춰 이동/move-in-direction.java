a18e690da2366922be8bedbd322d896b18976257
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-01-23 12:25:15 +0900

    Title: 방향에 맞춰 이동, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240123/방향에 맞춰 이동/move-in-direction.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 0, y = 0;
+        int[] dx = new int[]{1,  0, -1, 0};
+        int[] dy = new int[]{0, -1,  0, 1};
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0 ; i < n; i++) {
+            String d = sc.next();
+            int k = sc.nextInt();
+
+            if("E".equals(d)) {
+                x += k*dx[0]; y += k*dy[0];
+            }
+            else if("S".equals(d)) {
+                x += k*dx[1]; y += k*dy[1];
+            }
+            else if("W".equals(d)) {
+                x += k*dx[2]; y += k*dy[2];
+            }
+            else {
+                x += k*dx[3]; y += k*dy[3];
+            }
+        }
+    System.out.print(x + " " + y);
+
+    }
+
+}
