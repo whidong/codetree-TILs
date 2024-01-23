1b0f7b97556879d2999c09fe4c220fee9b701432
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-01-23 16:53:46 +0900

    Title: 세 정수형 변수 선언, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240123/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 7;
+        b = 23;
+        c = 30;
+        System.out.printf("%d + %d = %d", a, b, c);
+    }
+}
