e2b12a79f5288a4f4a2a15fe0cda93da85d522b4
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-01-30 13:41:39 +0900

    Title: 문자에 따른 명령 2, Time: 131ms, Memory: 73MB, Status: Passed - CodeTree

--- 240130/문자에 따른 명령 2/text-based-commands2.py
+++ 240130/문자에 따른 명령 2/text-based-commands2.py
@@ -1,4 +1,15 @@
-dx, dy = [1, -1, 0, 0], [0, 0, -1, 1]
+dirm = 3
+x, y =0, 0
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+k = list(input())
 
-k = list(input().split(''))
-print(k)
+for i in k:
+    if i == "L":
+        dirm = (dirm+4-1)%4 
+
+    elif i == "R":
+        dirm = (dirm+1)%4
+
+    elif i == "F":
+        x, y = x + dx[dirm], y + dy[dirm]
+print(x, y)
