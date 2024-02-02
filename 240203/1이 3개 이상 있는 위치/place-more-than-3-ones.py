99a668c01bb36c9cfc53c96f6e4d1e3cee8341e9
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-02-03 01:19:41 +0900

    Title: 1이 3개 이상 있는 위치, Time: 197ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240203/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+k =[]
+for i in range(n):
+    k.append(list(map(int, input().split())))
+
+def in_range(x, y, n):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+result = 0
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        for x, y in zip(dx, dy):    
+            nx, ny = x + i, y + j
+            if in_range(nx, ny, n) and k[nx][ny] == 1:
+                cnt += 1
+        if cnt >= 3:
+            result += 1
+
+print(result)
