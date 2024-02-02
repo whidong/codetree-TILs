b19f57001ec0ec44ef0ef55c8aa5fd672fd4b5e1
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-02-03 03:55:04 +0900

    Title: 행복한 수열의 개수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240203/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240203/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,19 +8,25 @@ for i in range(n):
     l_cnt = 0
     r_p =0
     l_p = 0
+    r_b = False
+    l_b = False
     for j in range(n):
         if k[i][j] == r_p:
             r_cnt += 1
+            if m <= r_cnt + 1:
+                r_b = True
         else:
             r_cnt = 0
         if k[j][i] == l_p:
             l_cnt += 1
+            if m <= l_cnt + 1:
+                l_b = True
         else:
             l_cnt = 0
         r_p = k[i][j]
         l_p = k[j][i]
-    if m <= r_cnt + 1:
+    if r_b == True:
         result += 1
-    if m <= l_cnt + 1:
+    if l_b == True:
         result += 1
 print(result)
