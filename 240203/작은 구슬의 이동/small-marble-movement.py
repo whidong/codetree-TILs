838cd6ace800f1786f86d48d3ef18c2f125bb0f4
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-02-03 02:08:51 +0900

    Title: 작은 구슬의 이동, Time: 127ms, Memory: 73MB, Status: Passed - CodeTree

--- 240203/작은 구슬의 이동/small-marble-movement.py
+++ 240203/작은 구슬의 이동/small-marble-movement.py
@@ -1,19 +1,15 @@
 def in_range(x, y, n):
-    return 0 <= x < n and 0 <= y < n
+    return 0 < x <= n and 0 < y <= n
 
 n, t= map(int, input().split())
 r, c, d = map(str, input().split())
-r, c = int(r)-1, int(c)-1
+r, c = int(r), int(c)
 dic = 0
 dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
-if d == "U":
-    dic = 1
-elif d == "D":
-    dic = 2
-elif d == "R":
-    dic = 0
-elif d == "L":
-    dic = 3
+
+mapper = {'R': 0,'D': 1,'U': 2,'L': 3}
+dic = mapper[d]
+
 for i in range(t):
     nx, ny = r + dxs[dic], c + dys[dic]
     if in_range(nx, ny, n):
@@ -22,4 +18,4 @@ for i in range(t):
         dic = 3 - dic
 
     
-print(r+1, c+1)
+print(r, c)
