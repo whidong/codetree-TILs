b10611910d531f9a83fe74f1815e376a205c33bb
KIM DONGHWI <99733260+whidong@users.noreply.github.com>
2024-04-23 16:51:58 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240423/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
+++ 240423/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -14,12 +14,12 @@ for j in k:
         now = now%4
     elif j == "R":
         now -= 1
-        now = now%4
+        now = now%5
     elif j == "F":
         if x + dx[now] == n or x + dx[now] < -1 or y + dy[now] == n or y + dy[now] < -1:
             pass
         else:
-            x = x + dx[now]
-            y = y + dy[now]
+            x += dx[now]
+            y += dy[now]
             result += int(box[x][y])
 print(result)
